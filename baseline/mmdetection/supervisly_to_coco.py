07cef3853916939f0e8abe0912e019930013d3cd
kangshwan <rnltls97@gmail.com>
2024-01-09 16:22:06 +0900

    Fix: 변수이름 명확히 변경, 함수이름 명확히 변경, 필요없는 argument 제거
    issue: #10

--- baseline/mmdetection/supervisly_to_coco.py
+++ baseline/mmdetection/supervisly_to_coco.py
@@ -10,7 +10,7 @@ def args_as_list(s):
     return v
 
 
-def canAppend(range_tuple, cur_idx, cur_img_id):
+def is_available(range_tuple, cur_idx, cur_img_id):
     for idx, (x, y) in enumerate(range_tuple):
         if idx != cur_idx:
             if x <= cur_img_id <= y:
@@ -20,15 +20,15 @@ def canAppend(range_tuple, cur_idx, cur_img_id):
 
 def convert(args):
     dir_paths = []
-    for output in args.outputs:
-        dir_paths.append(args.root + output)
+    for input in args.inputs:
+        dir_paths.append(args.root + input)
 
     with open(dir_paths[0], "r") as file:
         json_data = json.load(file)
 
     new_annotations = []
     annotation_id = 0
-    tmpSet = set()
+
     for idx, dir_path in enumerate(dir_paths):
         with open(dir_path, "r") as file:
             json_data = json.load(file)
@@ -49,11 +49,9 @@ def convert(args):
                 new_json_data = json_data
 
             for ann_num in range(len(json_data["annotations"])):
-                if canAppend(
+                if is_available(
                     args.range, idx, json_data["annotations"][ann_num]["image_id"]
                 ):
-                    if json_data["annotations"][ann_num]["image_id"] not in tmpSet:
-                        tmpSet.add(json_data["annotations"][ann_num]["image_id"])
                     json_data["annotations"][ann_num]["id"] = annotation_id
                     annotation_id += 1
                     new_annotations.append(json_data["annotations"][ann_num])
@@ -77,48 +75,42 @@ if __name__ == "__main__":
         help="dataset's location (default: ../../dataset/)",
     )
 
-    # # of outputs
+    # inputs
     parser.add_argument(
-        "--number_of_outputs",
-        type=int,
-        default=2,
-        help="enter number of supervisly output",
+        "--inputs",
+        type=args_as_list,
+        default=[],
+        help="Enter list of inputs ex) ['instances.json','instances2.json','instances.json']",
     )
 
-    # outputs
-    parser.add_argument(
-        "--outputs", type=args_as_list, default=[], help="Enter list of outputs"
-    )
-
-    # range of output
+    # range of input
     parser.add_argument(
         "--range",
         type=args_as_list,
         default=[],
-        help="Enter range of each output,\n\
-            ex) output1's range(0~1000), output2's range(1001~4883), input is [0,1000,1001,4883]",
+        help="Enter range of each input,\n\
+            ex) input1's range(0~1000), input2's range(1001~4883), \n\
+                input is [0,1600,1601,2400,2401,4883]",
     )
 
-    # nameof output
+    # name of output
     parser.add_argument(
         "--output", type=str, default="clean.json", help="name of output json file"
     )
+
     args = parser.parse_args()
 
-    outputs_range = []
+    inputs_range = []
 
     for i in range(1, len(args.range), 2):
-        outputs_range.append((args.range[i - 1], args.range[i]))
+        inputs_range.append((args.range[i - 1], args.range[i]))
 
-    args.range = outputs_range
-    if args.outputs == []:
-        raise Exception("Outputs are empty")
+    args.range = inputs_range
 
-    if args.number_of_outputs != len(args.outputs):
-        print(f"{args.number_of_outputs}!={len(args.outputs)}")
-        raise Exception("# of outputs and len(outputs) mismatch")
+    if args.inputs == []:
+        raise Exception("Inputs are empty")
 
-    if len(args.outputs) != len(args.range):
+    if len(args.inputs) != len(args.range):
         raise Exception("len(outputs) and len(range) mismatch")
 
     convert(args)
